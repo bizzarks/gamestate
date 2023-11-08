54df3292902ee2d7006ba207d1ceef383801516d
bizzarks <bizzarks@hotmail.com>
2023-11-07 19:39:44 -0500

    pushed up some inverted player box stuff

--- app/player.js
+++ app/player.js
@@ -1,14 +1,18 @@
-import styles from './page.module.css'
+import styles from './player.module.css'
 
 
 export default function PlayerLife(player) {
     if (player.inverted == 'true') {
         return (
-            <div className={styles.plife}>
-                <div className={styles.player_name_blank}></div>
-                <div className={styles.p_life_total_inverted}>{player.lifeTotal}</div>
-                <div className={styles.player_name_inverted}>{player.name}</div>
-            </div>
+                <div className={styles.plife_inverted}>
+                    <div className={styles.player_name}>{player.name}</div>
+                    <div className={styles.p_life_total}>{player.lifeTotal}</div>
+                    <div className={styles.player_name_blank}></div>
+                    <div className={styles.life_button_container}>
+                        <button className={styles.life_up_button}></button>
+                        <button className={styles.life_down_button}></button>
+                    </div>
+                </div>
         )
     } else {
         return (
@@ -16,6 +20,10 @@ export default function PlayerLife(player) {
                 <div className={styles.player_name}>{player.name}</div>
                 <div className={styles.p_life_total}>{player.lifeTotal}</div>
                 <div className={styles.player_name_blank}></div>
+                <div className={styles.life_button_container}>
+                    <button className={styles.life_down_button}></button>
+                    <button className={styles.life_up_button}></button>
+                </div>
             </div>
         )
     }
